a6d6cf47dc3a43b4849babd3309aa8fe60e4e630
Giulio Costantini <costantinigiulio@gmail.com>
2018-02-12 16:41:38 +0100

    Excluding participants with too few responses is now optional, implemented in option autoremove Also replaced calls to rbind_all() with bind_rows()

---
+++ NEWS.md
@@ -0,0 +1,10 @@
+# IATscores 0.2.0
+
+* Excluding participants with too few responses is now optional, implemented in option autoremove
+
+* replaced calls to rbind_all() with bind_rows()
+
+* Removed dependency on nem to prevent issues during installation
+
+* bug fix when P4 = "dist". thanks to Geraldine Escriva
+
--- R/RobustScores.R
+++ R/RobustScores.R
@@ -6,7 +6,8 @@ RobustScores <- function(IATdata,
                          P3 = c("dscore", "gscore", "wpr90", "minid",
                                 "minid_t10", "minid_w10", "minid_i10"),
                          P4 = c("nodist", "dist"),
-                         maxMemory = 1000, verbose = TRUE)
+                         maxMemory = 1000, verbose = TRUE,
+                         autoremove = TRUE)
 {
    
   mincor <- 3 # minimum number of correct responses with lat < k10
@@ -82,7 +83,7 @@ RobustScores <- function(IATdata,
                           latency >= lofxtrim)) %>%
     filter(ncor < mincor)
   
-  if(nrow(ncor) != 0)
+  if(autoremove & nrow(ncor) != 0)
   {
     IATdata <- filter(IATdata, !subject %in% ncor$subject)
     warning(paste("The following subjects have been removed because they
